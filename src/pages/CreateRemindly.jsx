0b4a41eb9178eb714db501f5a7c2b970fc9bcb8e
cmcodedby <cartier.medina@codedbykids.org>
2026-05-16 13:15:53 -0400

    Co-authored-by: sb-cb202 <sb-cb202@users.noreply.github.com>

--- src/pages/CreateRemindly.jsx
+++ src/pages/CreateRemindly.jsx
@@ -1,6 +1,25 @@
-export const CreateRemindly = () => {
+import {useState} from 'react'
 
+// Create a variable for your backend URL
+
+const URL = 'https://my-node-app-xirq.onrender.com'
+
+export const CreateRemindly = () => {
+    // State variable to store reminder
+    const [reminder, storeReminder] = useState([])
+    // State variable to store loading state
+    const [loading, editReminder] = useState([])
+    // handleSubmit() function to send POST request
+    
+    // return HTML
     return (
-        <div>Create Remindly</div>
+        // container div
+   <d></d>
+            // header elem that says "Add reminder"
+
+            // input elem for user to add reminder
+
+            // button to add reminder (sends post request with onClick)
+           
     )
 }
